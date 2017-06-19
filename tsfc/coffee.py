b868e7c7f64205588a8d0f640df3c4c69e7afc10
NicholasBermuda <snicholasbarton@gmail.com>
2017-06-19 12:31:06 +0100

    isnan to complex version

--- tsfc/coffee.py
+++ tsfc/coffee.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import, print_function, division
 
 from collections import defaultdict
 from functools import reduce
-from math import isnan
+from cmath import isnan
 import itertools
 
 import numpy
