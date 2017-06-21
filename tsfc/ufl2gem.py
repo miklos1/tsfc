751ff4a884b63246d6bbfdbd3a7a430360a080c8
NicholasBermuda <snicholasbarton@gmail.com>
2017-06-21 16:45:14 +0100

    add ufl translations for real and imag

--- tsfc/ufl2gem.py
+++ tsfc/ufl2gem.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, print_function, division
 import collections
 import ufl
 
-from gem import (Literal, Zero, Identity, Sum, Product, Division, Conj,
+from gem import (Literal, Zero, Identity, Sum, Product, Division, Real, Imag,
                  Power, MathFunction, MinValue, MaxValue, Comparison,
                  LogicalNot, LogicalAnd, LogicalOr, Conditional,
                  Index, Indexed, ComponentTensor, IndexSum,
@@ -45,6 +45,20 @@ class Mixin(object):
 
     def division(self, o, numerator, denominator):
         return Division(numerator, denominator)
+
+    def real(self, o, expr):
+        if o.ufl_shape:
+            indices = tuple(Index() for i in range(len(o.ufl_shape)))
+            return ComponentTensor(Real(Indexed(expr, indices)), indices)
+        else:
+            return Real(expr)
+
+    def imag(self, o, expr):
+        if o.ufl_shape:
+            indices = tuple(Index() for i in range(len(o.ufl_shape)))
+            return ComponentTensor(Imag(Indexed(expr, indices)), indices)
+        else:
+            return Imag(expr)
 
     def conj(self, o, expr):
         if o.ufl_shape:
