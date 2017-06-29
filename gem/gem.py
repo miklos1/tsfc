4329218e647761a74a7b50f4be4e2c48641d30e7
NicholasBermuda <snicholasbarton@gmail.com>
2017-06-29 19:41:25 +0100

    more changes in type checking

--- gem/gem.py
+++ gem/gem.py
@@ -27,6 +27,8 @@ from numpy import asarray
 
 from gem.node import Node as NodeBase
 
+from tsfc.parameters import numpy_type
+
 
 __all__ = ['Node', 'Identity', 'Literal', 'Zero', 'Failure', 'ComplexPartsFunction', 
            'Variable', 'Sum', 'Product', 'Division', 'Power', 
@@ -161,7 +163,7 @@ class Literal(Constant):
             return super(Literal, cls).__new__(cls)
 
     def __init__(self, array):
-        self.array = asarray(array, dtype='complex128')
+        self.array = asarray(array, dtype=numpy_type()) # import numpy_type()
 
     def is_equal(self, other):
         if type(self) != type(other):
